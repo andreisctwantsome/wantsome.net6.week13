a694d125b9979a571e16545e4deb08d0843d6744
sctrandrei <sctr.andrei@gmail.com>
2020-10-05 19:05:31 +0300

    add first application

---
+++ Week13/WebApplicationOnFramework/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationOnFramework.Models;
+
+namespace WebApplicationOnFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        // /Home/Index
+        public ActionResult Index()
+        {
+            // search a view Index in Home folder
+            return View();
+        }
+
+        // /Home/IndexWithId/2
+        public ActionResult IndexWithId(int id)
+        {
+            return View("Index");
+        }
+
+        // /Home/About
+        public ActionResult About()
+        {
+            ViewBag.Message = "About page!!";
+
+            return View();
+        }
+
+        // /Home/Contact
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            var manager = new ContactManager();
+
+            var contact = manager.GetDefaultContact();
+
+            var view = View(contact);
+
+            return view;
+        }
+    }
+}
---
+++ Week13/WebApplicationOnFramework/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationOnFramework.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+    }
+
+    public class ContactManager
+    {
+        public Contact GetDefaultContact()
+        {
+            return new Contact()
+            {
+                Name = "Andrei"
+            };
+        }
+    }
+}
